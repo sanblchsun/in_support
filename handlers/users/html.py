04b89afe285ff1437cf0ea55d63b9e1c80a17ccd
Makosov Aleksandr <logovopost@yandex.ru>
2025-12-01 21:26:45 +0300

    еще исправление html.py

--- handlers/users/html.py
+++ handlers/users/html.py
@@ -1,13 +1,16 @@
-from datetime import datetime
+import html
 
 
-def get_html(description=' ',
-             priority=' '):
-    html = f"""
+def get_html(description=' ', priority=' '):
+
+    description = html.escape(description)
+    priority = html.escape(priority)
+    description = html.escape(description.strip())
+
+    html_text = f"""
 <i><b>Описание проблемы:</b></i>
 <code>{description}</code>
 <i><b>Приоритет заявки:</b></i>
 <code>{priority}</code>
 """
-    return html
-
+    return html_text
