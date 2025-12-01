776dbc2dcaa86d323149562b46e1ebc8e1bb2b39
Ваше Имя <you@example.com>
2025-12-01 15:08:39 +0300

    fix error in handlers/users/html.py

--- handlers/users/html.py
+++ handlers/users/html.py
@@ -2,21 +2,12 @@ from datetime import datetime
 
 
 def get_html(description=' ',
-             priority=' ',
-             comment_from_bot='----------',
-             status_request=' ',
-             number_request=' '):
+             priority=' '):
     html = f"""
 <i><b>Описание проблемы:</b></i>
 <code>{description}</code>
 <i><b>Приоритет заявки:</b></i>
 <code>{priority}</code>
 """
- # 1c integrated
-# <i><b>Статус заявки:</b></i>
-# <code>{status_request}</code>
-# <i><b>Комментарии от бота:</b></i>
-# <code>{comment_from_bot}</code>
-# 1c integrated
     return html
 
