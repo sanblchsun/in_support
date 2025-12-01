1285d7eda035694b9701ce503452d1cc75cc0778
Sanblch <logovopost@yandex.ru>
2025-12-01 20:55:51 +0300

    fix error user/html.py

--- handlers/users/html.py
+++ handlers/users/html.py
@@ -1,22 +1,17 @@
-from datetime import datetime
+import html
 
 
-def get_html(description=' ',
-             priority=' ',
-             comment_from_bot='----------',
-             status_request=' ',
-             number_request=' '):
-    html = f"""
+def get_html(description=' ', priority=' '):
+
+    description = html.escape(description)
+    priority = html.escape(priority)
+    description = html.escape(description.strip())
+
+    html_text = f"""
 <i><b>Описание проблемы:</b></i>
 <code>{description}</code>
 <i><b>Приоритет заявки:</b></i>
 <code>{priority}</code>
 """
- # 1c integrated
-# <i><b>Статус заявки:</b></i>
-# <code>{status_request}</code>
-# <i><b>Комментарии от бота:</b></i>
-# <code>{comment_from_bot}</code>
-# 1c integrated
-    return html
+    return html_text
 
